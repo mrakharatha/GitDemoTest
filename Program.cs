04f9d1520e2cc4b748d450718f87650f6bdad1dd
Mrakharatha78 <Mrakharatha78@gmail.com>
2020-06-20 10:31:32 +0430

    change Line 2

--- Program.cs
+++ Program.cs
@@ -7,6 +7,7 @@ namespace GitDemoTest
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("New GitHub");
         }
     }
 }
